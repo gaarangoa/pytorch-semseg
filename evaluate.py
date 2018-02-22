65fc93bbf2726ae508078f81ffea1ced0eda0591
gaarangoa <gaarangoa@gmail.com>
2018-02-22 17:32:36 -0500

    push

---
+++ evaluate.py
@@ -0,0 +1,18 @@
+import sys
+import torch
+import visdom
+import argparse
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+from torch.autograd import Variable
+from torch.utils import data
+from tqdm import tqdm
+
+from ptsemseg.models import get_model
+from ptsemseg.loader import get_loader, get_data_path
+from ptsemseg.metrics import runningScore
+from ptsemseg.loss import *
+from ptsemseg.augmentations import *
