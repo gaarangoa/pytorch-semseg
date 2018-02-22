de2f2a6dc47d634af33aeb8ee89e138ee0e83f04
gaarangoa <gaarangoa@gmail.com>
2018-02-22 17:39:55 -0500

    fix

--- evaluate.py
+++ evaluate.py
@@ -16,3 +16,99 @@ from ptsemseg.loader import get_loader, get_data_path
 from ptsemseg.metrics import runningScore
 from ptsemseg.loss import *
 from ptsemseg.augmentations import *
+
+
+# Setup Augmentations
+data_aug= Compose([RandomRotate(10),                                        
+                    RandomHorizontallyFlip()])
+
+# Setup Dataloader
+
+args_dataset = "pascal"
+args_img_rows = 128
+args_img_cols = 128
+args_batch_sise = 10
+args_arch = "unet"
+args_l_rate = 0.01
+args_resume = None
+args_n_epoch = 10
+
+print("loading dataset")
+
+data_loader = get_loader(args_dataset)
+data_path = get_data_path(args_dataset)
+t_loader = data_loader(data_path, is_transform=True, img_size=(args_img_rows, args_img_cols), augmentations=data_aug)
+v_loader = data_loader(data_path, is_transform=True, split='val', img_size=(args_img_rows, args_img_cols), augmentations=data_aug)
+
+print("dataset read: loading data")
+n_classes = t_loader.n_classes
+trainloader = data.DataLoader(t_loader, batch_size=args_batch_size, num_workers=8, shuffle=True)
+valloader = data.DataLoader(v_loader, batch_size=args_batch_size, num_workers=8)
+
+# Setup Metrics
+running_metrics = runningScore(n_classes)
+    
+# Setup visdom for visualization
+if args.visdom:
+    vis = visdom.Visdom()
+
+    loss_window = vis.line(X=torch.zeros((1,)).cpu(),
+                        Y=torch.zeros((1)).cpu(),
+                        opts=dict(xlabel='minibatches',
+                                    ylabel='Loss',
+                                    title='Training Loss',
+                                    legend=['Loss']))
+
+# Setup Model
+model = get_model(args_arch, n_classes)
+
+model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
+model.cuda()
+
+# Check if model has custom optimizer / loss
+if hasattr(model.module, 'optimizer'):
+    optimizer = model.module.optimizer
+else:
+    optimizer = torch.optim.SGD(model.parameters(), lr=args_l_rate, momentum=0.99, weight_decay=5e-4)
+
+if hasattr(model.module, 'loss'):
+    print('Using custom loss')
+    loss_fn = model.module.loss
+else:
+    loss_fn = cross_entropy2d
+
+if args_resume is not None:                                         
+    if os.path.isfile(args_resume):
+        print("Loading model and optimizer from checkpoint '{}'".format(args_resume))
+        checkpoint = torch.load(args_resume)
+        model.load_state_dict(checkpoint['model_state'])
+        optimizer.load_state_dict(checkpoint['optimizer_state'])
+        print("Loaded checkpoint '{}' (epoch {})"                    
+                .format(args.resume, checkpoint['epoch']))
+    else:
+        print("No checkpoint found at '{}'".format(args_resume)) 
+
+best_iou = -100.0 
+for epoch in range(args_n_epoch):
+    model.train()
+    for i, (images, labels) in enumerate(trainloader):
+        images = Variable(images.cuda())
+        labels = Variable(labels.cuda())
+
+        optimizer.zero_grad()
+        outputs = model(images)
+
+        loss = loss_fn(input=outputs, target=labels)
+
+        loss.backward()
+        optimizer.step()
+
+        if args.visdom:
+            vis.line(
+                X=torch.ones((1, 1)).cpu() * i,
+                Y=torch.Tensor([loss.data[0]]).unsqueeze(0).cpu(),
+                win=loss_window,
+                update='append')
+
+        if (i+1) % 20 == 0:
+            print("Epoch [%d/%d] Loss: %.4f" % (epoch+1, args_n_epoch, loss.data[0]))
